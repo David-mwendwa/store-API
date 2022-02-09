8ba3da4e5b37c0051dea0d38ec5013454cc8f805
David-mwendwa <klatzdavie19@gmail.com>
2022-02-09 08:56:49 +0300

    connect mongo DB

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+require('dotenv').config()
+// async errors
+
+const express = require('express')
+const app = express();
+
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API<a href="/api/v1/products">products route</a></h1>')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+// products route
+app.use(notFoundMiddleware);
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 5000
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`server listening on port ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
