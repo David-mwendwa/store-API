ea41b4ae169c113f206b5842b718bc7128c60d0e
David-mwendwa <klatzdavie19@gmail.com>
2022-02-10 08:30:19 +0300

    add product selection to getAllProducts

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price')
+  const products = await Product.find({}).select('name price')
   res.status(200).json({nbHits:products.length, products})
 }
 
 const getAllProducts = async (req, res) => {
-  const {featured, company, name} = req.query
+  const {featured, company, name, sort, fields} = req.query
   const queryObject = {}
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
@@ -17,12 +17,19 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
+
   let result = Product.find(queryObject)
+
   if (sort) {
     const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
   } else {
     result = result.sort('createdAt')
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList);
   }
   const products = await result
   res.status(200).json({nbHits:products.length, products})
