2fe25178a8f114d8339da54875873b33a2d2a8ee
David-mwendwa <klatzdavie19@gmail.com>
2022-02-10 08:52:17 +0300

    create pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,42 +1,52 @@
-const Product = require('../models/product')
+const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price')
-  res.status(200).json({nbHits:products.length, products})
-}
+  console.log(req.query);
+  const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10).skip(10)
+  res.status(200).json({ nbHits: products.length, products });
+};
 
 const getAllProducts = async (req, res) => {
-  const {featured, company, name, sort, fields} = req.query
-  const queryObject = {}
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
   if (featured) {
-    queryObject.featured = featured === 'true' ? true : false
+    queryObject.featured = featured === 'true' ? true : false;
   }
   if (company) {
-    queryObject.company = company
+    queryObject.company = company;
   }
   if (name) {
-    queryObject.name = { $regex: name, $options: 'i' }
+    queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  let result = Product.find(queryObject)
+  let result = Product.find(queryObject);
 
   if (sort) {
-    const sortList = sort.split(',').join(' ')
-    result = result.sort(sortList)
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
   } else {
-    result = result.sort('createdAt')
+    result = result.sort('createdAt');
   }
 
   if (fields) {
-    const fieldsList = fields.split(',').join(' ')
+    const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
-  const products = await result
-  res.status(200).json({nbHits:products.length, products})
-}
+
+  // pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+  results = result.skip(skip).limit(limit)
+
+  const products = await result;
+  res.status(200).json({ nbHits: products.length, products });
+};
 
 module.exports = {
   getAllProducts,
-  getAllProductsStatic
-}
-
+  getAllProductsStatic,
+};
