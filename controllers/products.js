3b1a720eb3fa3ce4b51fc1ab94e64db53d763c0e
David-mwendwa <klatzdavie19@gmail.com>
2022-02-10 09:21:27 +0300

    set up complex numeric filters in the api

--- controllers/products.js
+++ controllers/products.js
@@ -10,7 +10,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -21,6 +21,27 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
+
+  // numeric filter - <,>,<=,>=,=
+  if(numericFilters) {
+    const operatorMap = {
+      '>':'$gt',
+      '>=':'$gte',
+      '=':'$eq',
+      '<':'$lt',
+      '<=':'$lte',
+    }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach(item => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = {[operator]:Number(value)}
+      }
+    })
+  }
+  console.log(queryObject)
 
   let result = Product.find(queryObject);
 
